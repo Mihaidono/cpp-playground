4b7c63f7f60367af707c84179684174b5f1c553e
Mihaidono <amihai2001@gmail.com>
2023-08-02 10:48:22 +0300

    Added name in 01HW and implemented functionality in 02OddEven

--- 02_odd_even/src/main.cpp
+++ 02_odd_even/src/main.cpp
@@ -28,30 +28,23 @@ int main(int argc, char *argv[])
 	//		  02_odd_even.exe 2		=> Output: EVEN
 	//		  02_odd_even.exe 		=> Output: No program arguments found.
 	//		  02_odd_even.exe ABC   => Undefined output (do whatever).
-	//		
+	//
 
 	// Make sure there are some program arguments available.
 	if (argc <= 1)
 	{
 		printf("No program arguments found.\n");
+		return -1;
 	}
-	
+
 	// TODO(Gusti): i don't know why this doesn't work, but someone please FIX it.
-	// --------------- start
+	// I made it so it accepts multiple arguments and prints for each of them if they're odd or even
+	for (int i = 1; i < argc; i++)
+	{
+		printf("For argument: %s it is:", argv[i]);
+		printOddOrEven(atoi(argv[i]));
 
-	// Get the first argument
-	std::string argumentAsString = argv[1];
-	const char* argumentAsCharArray = argumentAsString.c_str();
+	}
 
-	//number = argv[1]; // No
-	//should use atoi?
-	// or std::stoi?
-
-	std::cout << argumentAsString << std::endl; // i think this should be removed
-
-	// --------------- stop
-
-	printOddOrEven(number);
-
-	return 0;
+	return 1;
 }
